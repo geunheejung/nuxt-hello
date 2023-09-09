b75959665cc4f4f3fdc7d51b27cba7d2c224f00e
geunheejung <geunhee0212@gmail.com>
2023-09-09 09:01:47 +0900

    챕터 2.

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <header>
+      <ul>
+        <li><nuxt-link to="/main">홈페이지</nuxt-link></li>
+        <li><nuxt-link to="/main">메인 페이지</nuxt-link></li>
+        <li><nuxt-link to="/product">상품 페이지</nuxt-link></li>
+      </ul>
+    </header>
+      <Nuxt />
+  </div>
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
+    // 애플리케이션에 대한 모든 기본 메타 태그를 정의할 수 있다.
     title: 'nuxt-tuto',
     htmlAttrs: {
       lang: 'en',
@@ -9,6 +10,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
+      // 아이폰 사파리 -> 숫자를 전화번호로 자동 인식 -> 자동링크 해제
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
@@ -17,6 +19,7 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
+  // 루트 Vue.js 애플리케이션을 인스턴스화 하기 전에 실행해야 하는 JavaScript 플러그인을 정의 가능.
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
@@ -33,5 +36,11 @@ export default {
   modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    transpile: [({ isLegacy }) => isLegacy && 'axios'],
+  },
+  //  server setup
+  server: {
+    port: 8080,
+  },
 }
